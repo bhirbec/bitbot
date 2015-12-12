7e66f4f15e0f9a51f2008fea5949bba7c24c77cc
Benoit Hirbec <benoit@gardentechno.com>
2015-12-11 23:51:04 -0800

    Add a script to provision and setup a GCE instance

---
+++ ansible/setup.sh
@@ -0,0 +1,18 @@
+# TODO: check --maintenance-policy
+# TODO: check --scopes
+
+gcloud compute instances create recorder \
+    --zone "us-central1-b" \
+    --machine-type "f1-micro" \
+    --maintenance-policy "MIGRATE" \
+    --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/ubuntu-1404-trusty-v20150805" \
+    --boot-disk-size "10" \
+    --boot-disk-type "pd-standard";
+
+# make sure we can ssh the instance
+while [ "$(gcloud compute ssh recorder --command="echo ok")" != "ok" ]; do
+    echo "Cannot SSH the instance. Retrying in 1 sec...";
+    sleep 1;
+done
+
+ansible-playbook ansible/setup.yaml -i ansible/gce_hosts
